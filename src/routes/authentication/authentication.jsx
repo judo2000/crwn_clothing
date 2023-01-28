06cd3da045b79bda55160515157d7c4e2706c3e2
Stephen Moore <judo2000@comcast.net>
2023-01-28 15:16:16 -0700

    Create sign in component and rename sign up route to authentication

--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -1,23 +1,18 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
-const Signin = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
+const Authentication = () => {
   return (
     <div>
       <h1>Sign in Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default Signin;
+export default Authentication;
