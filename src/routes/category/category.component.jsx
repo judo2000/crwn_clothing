a98724ed2786edf06a458c3f6a4d3ff76b3f21b7
Stephen Moore <judo2000@comcast.net>
2023-02-03 14:39:29 -0700

    Further edited to memoize categories selector

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -7,11 +7,12 @@ import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
+  console.log('rendering/re-rendering category component');
   const categoriesMap = useSelector(selectCategoriesMap);
-
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
+    console.log('effect fired calling setProducts');
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
